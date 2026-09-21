61f2dc94e4023394d98c7a021f7051a319506098
arnaldo nascimento <popoarnaldo@hotmail.com>
2026-09-20 21:38:57 -0300

    Chess layer and printing the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -23,7 +23,17 @@ public class Board {
     public int setRows(int rows) {
         return this.rows = rows;
     }
+
     public int setColumns(int columns) {
         return this.columns = columns;
     }
+
+    public Piece piece(int row, int column) {
+        return pieces[row][column];
+    }
+
+    public Piece piece(Position position) {
+        return pieces[position.getRow()][position.getColumn()];
+    }
+    
 }
